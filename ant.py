153eb38d29885696c44406ea35e3514d56000884
Mira Flynn <miraflynn0@gmail.com>
2023-02-11 15:24:45 -0500

    A bunch of docstrings in ant.py. Committing for lunch break

--- ant.py
+++ ant.py
@@ -2,11 +2,21 @@ import numpy as np
 from scipy.signal import convolve2d
 import random
 class Ant:
-    """Initialize the ant
-    sim is the simulation the ant is a part of (for calling simulation modifying functions)
-    See ant_simulation for parameter definitions
-    """
     def __init__(self, sim, fidelity_min, fidelity_max, phermone_max, trail_level):
+        """Initialize the ant.
+
+        Note: Type and range restrictions are included in docstrings, but are not enforced. If these bounds are not followed, unexpected behavior may occur.
+
+        Args:
+            self
+            sim: the Simulation object the ant is a part of (for calling simulation modifying functions)
+            fidelity_min: float decimal between 0 and 1, the minimum chance of self.fidelity_test() returning true
+            fidelity_max: float decimal between 0 and 1, the maximum chance of self.fidelity_test() returning true
+            phermone_max: integer, the maximum quantity of phermones that the ant can sense
+            trail_level: integer, the minimum phermone level that the ant will consider to be following a trail
+        Returns:
+            none
+        """
         self.location = np.int_(np.round(np.divide(sim.array.shape,2)))
         self.following = False
         self.fidelity_min = fidelity_min
@@ -15,88 +25,121 @@ class Ant:
         self.sim = sim
         self.trail_level = trail_level
 
-        # Directions that the ant can be pointing
+        # Directions that the ant can be pointing, in vector form
         self.directions = [(-1,-1),(-1,0),(-1,1),(0,1),(1,1),(1,0),(1,-1),(0,-1)]
         # Directions Indices
         # 012
         # 7 3
         # 654
 
-        # start in a random direction
+        # Start in a random direction
         self.direction = np.random.randint(0,8)
-        # self.direction = 5
-        self.neighbor_kernel = np.ones((3,3))
-        self.neighbor_kernel[1,1] = 0
-        # self.sim.deposit_phermone(self.location)
-        # self.move_forward()
-        # print(self.neighbor_kernel)
 
-    """Make a decision based on the local phermone count and the fidelity
-
-    returns: boolean choice
-    """
     def fidelity_test(self):
+        """Make a decision based on the local phermone count and the fidelity.
+
+        Note: The probability of returning True is bound by self.fidelity_min and self.fidelity_max.
+        The probability is the local phermone level divided by self.phermone_max.
+
+        Args:
+            self
+        Returns:
+            a boolean representing the result of the ant's fidelity check.
+        """
+
+        # Get local pher_level
         pher_level = self.sim.array[*self.location]
+
+        # Probability of True is phermone level divided by self.phermone_max
         pher_frac = pher_level/self.phermone_max
-        # print(self.sim.array)
-        # print(pher_level)
+
+        # That probability is bound by self.fidelity_min and self.fidelity_max
         pher_frac = min(self.fidelity_max,pher_frac)
         pher_frac = max(self.fidelity_min,pher_frac)
+
+        # Return True with probability pher_frac
         return (np.random.choice([True, False], p=[pher_frac,1-pher_frac]))
 
-    """Get location of each neighbor
-
-    returns: a list of 2-length tuples specifying neighbor locations
-    """
     def get_neighbors(self):
-        location_array = np.zeros_like(self.sim.array)
-        # print(self.location)
-        location_array[*self.location] = 1
-        neighbors = convolve2d(location_array, self.neighbor_kernel, mode='same').nonzero()
-        return list(zip(neighbors[0],neighbors[1]))
+        """Get location of all neighbors.
 
-    """Return the list of neighbors, ordered by phermones.
+        Args:
+            self
+        Returns:
+            a list of 2-length arrays specifying neighbor locations
+        """
 
-    get_phermones_per_neighbor(self.get_neighbors())[0][0] will be the location of the highest phermone neighbor
+        # Element-wise add self.location and direction, make a list iterating over directions
+        return [np.add(self.location, self.directions[i]) for i in range(0,8)]
 
-    returns: a list of 2-length tuples of 2-length location tuples and corresponding phermone levels, sorted with highest level first
-    """
     def get_phermones_per_neighbor(self, possible_locs):
-        # print(self.get_neighbors())
+        """Return the list of neighbors, ordered by the phermone level at those locations.
+
+        get_phermones_per_neighbor(self.get_neighbors())[0][0] will be the location of the highest phermone neighbor (although may be tied with others)
+
+        Args:
+            possible_locs: an iterable of 2-length iterables representing each possible location
+        Returns:
+            a list of 2-length tuples of 2-length location tuples and corresponding phermone levels, sorted with highest level first
+        """
+
+        # array to append to
         phermone_levels = []
+
+        # for each possible location
         for l in possible_locs:
+            # If the possible location is within the bounds, then...
+            # This should always be true because the ant should never be allowed on the edge of the space, but it's here for safety
             if(l[0] > 0 and l[1] > 0 and l[0] <= self.sim.array.shape[0]-1 and l[1] <= self.sim.array.shape[1]-1):
+                # level at location is the phermone array, limited by self.phermone_max
                 loc_level = min(self.sim.array[*l],self.phermone_max)
+                # and append
                 phermone_levels.append((l,loc_level))
 
-        # _ = [print(self.sim.array[l]) for l in self.get_neighbors()]
+
         phermone_levels.sort(key=lambda level:level[1],reverse=True)
-        # print(phermone_levels[0][0])
-        # print(phermone_levels)
         return phermone_levels
 
-    """Move forward (toward current direction)
-    """
     def move_forward(self):
+        """Move forward (toward current direction)
+
+        Args:
+            self
+        Returns:
+            2-length array self.location
+        """
+        # element-wise add current location and direction vector of current direction
         self.location = np.add(self.location, self.directions[self.direction])
-        # print(self.location)
         return self.location
 
     def calc_direction(self, test_location):
+        """Calculate what direction a test location is from ant's current location
 
-        direction = np.where(self.directions == np.subtract(test_location,self.location))[0][0]
-        x = np.where(self.directions == np.subtract(test_location,self.location), 1, 0)
+        Args:
+            self
+            test_location: a 2-length array
+        Returns
+            2-length array, the direction of the test location
+        """
+
+        # Get a list of 2-length boolean vectors of whether each direction matches the difference between the two locations
+        # Not gonna lie, this is a bit of code magic I don't fully understand
         direction_bool_vecs = self.directions == np.subtract(test_location,self.location)
+        # Create a new list of whether both booleans are true
         direction_bools = [i[0] and i[1] for i in direction_bool_vecs]
+        # Direction is where value is true
         direction = np.where(direction_bools)[0][0]
-        # print(direction)
-        # if(x[0]==1 and x[1]==1):
-            # print(self.location, test_location, direction)
         return direction
 
 
 
-    def move_follow_new(self):
+    def move_follow_braden(self):
+        """This was created to test my following code, and is me directly coding from the notes in Braden's presentation.
+
+        I did this because I was troubleshooting my following algorithm and I was peer reviewing his presentation. I don't claim this as orignal.
+        In the end, this ended up having the same behavior as my fixed following algorithm.
+        I have not properly commented this as it is not "production" code. I just didn't want to delete it.
+        """
         possible_directions = [self.directions[(self.direction + i) % 8] for i in [-1,0,1]]
         # print(possible_directions)
         possible_neighbors = [np.add(self.location, d) for d in possible_directions]
@@ -133,7 +176,7 @@ class Ant:
 
     """Move while following
     """
-    def move_follow_old(self):
+    def move_follow(self):
         new_location = np.add(self.location, self.directions[self.direction])
         if(self.sim.array[*new_location] < self.trail_level):
             # print(self.direction)
@@ -162,8 +205,6 @@ class Ant:
             else:
                 return self.move_explore()
 
-    def move_follow(self):
-        self.move_follow_old()
 
     """Move while exploring (random turn)
     """
